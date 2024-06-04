1fe06c58bdd5e66d050b8c967d010c2ca81d1af2
Eris9 <cool5tarxv@gmail.com>
2024-06-04 19:21:52 +0800

    autopush

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 function blog(args){
     var ret = args.get("post")
-    template = new TemplateManager()
+    template = new TemplateManager("wynwxst/nss-demo")
     return template.load_template("blog.html",{"Date":"xx.xx.xx","Title":":)","Contents":String(ret)})
 
 }
